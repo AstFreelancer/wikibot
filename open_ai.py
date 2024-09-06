f52048fe4bb924f81741e3ec69eea47e3ff5b183
AstFreelancer <astfreelancer@mail.ru>
2024-09-06 20:38:41 +0400

    обновлена версия

--- open_ai.py
+++ open_ai.py
@@ -1,17 +1,20 @@
-import openai
+from openai import OpenAI
 from config import Config
 import base64
 
 config = Config()
-openai.api_key = config.openai_key
 
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=config.openai_key,
+)
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 # Получаем ответ от OpenAI API
 def get_openai_response(url: str):
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=[
             {
