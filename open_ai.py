797a159e269e9f7b01f45dae6c3b8d559ea12450
AstFreelancer <astfreelancer@mail.ru>
2024-11-19 20:51:53 +0400

    fix max tokens count

--- open_ai.py
+++ open_ai.py
@@ -36,7 +36,7 @@ def get_openai_response(url: str = None, prompt_text: str = None):
                     "content": content,
                 }
             ],
-            max_tokens=300,
+            max_tokens=1000,
         )
    #     print(response)
         # Проверка ответа на наличие необходимых данных
